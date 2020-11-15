aed89aa17f2edac8032ba94c472276a4aebc50d1
take0605 <take06051905@gmail.com>
2020-11-15 11:53:05 +0900

    更新

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { cleanup } from '@testing-library/react';
+import { useState } from 'react'
+const App = () => {
+  const name = 'yamane';
+  const [count, setCount] = useState(0);
 
-function App() {
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
+  }
+  const decrement = () => {
+    setCount(count - 1);
+    console.log(count);
+  };
+  const reset = () => {
+    setCount(0);
+    console.log(count);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
+      <h2>Hello world</h2>
+      <h3>Hello world</h3>
+      <h4>Hello world</h4>
+
+      <button onClick={() => window.alert("hello world")}>button</button>
+      <input type="text" />
+      <a href="#">a タグ</a>
+      {name}
+      <div><button onClick={increment}>いいね！</button><button onClick={decrement}>よくないね！</button>
+        <button onClick={reset}>リセット</button>
+        <p>{count}</p></div>
     </div>
   );
 }
