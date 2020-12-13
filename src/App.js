d532c26f11a30390994fa099cccdac98f96cb6a7
take0605 <take06051905@gmail.com>
2020-12-13 14:19:28 +0900

    課題前コミット

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import { cleanup } from '@testing-library/react';
-import { useState } from 'react'
-const App = () => {
-  const name = 'yamane';
-  const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-    console.log(count);
-  }
-  const decrement = () => {
-    setCount(count - 1);
-    console.log(count);
-  };
-  const reset = () => {
-    setCount(0);
-    console.log(count);
-  };
 
+import React from 'react';
+import './App.css';
+import Card from './Card';
+
+const sampleArray = [
+  { id: 1, name: 'aaa', date: 19920527 },
+  { id: 2, name: 'bbb', date: 19920528 },
+  { id: 3, name: 'ccc', date: 19920529 },
+  { id: 4, name: 'ddd', date: 19920531 },
+  { id: 5, name: 'eee', date: 19920501 }
+];
+
+
+
+function App() {
   return (
     <div>
-      <h1>Hello world</h1>
-      <h2>Hello world</h2>
-      <h3>Hello world</h3>
-      <h4>Hello world</h4>
 
-      <button onClick={() => window.alert("hello world")}>button</button>
-      <input type="text" />
-      <a href="#">a タグ</a>
-      {name}
-      <div><button onClick={increment}>いいね！</button><button onClick={decrement}>よくないね！</button>
-        <button onClick={reset}>リセット</button>
-        <p>{count}</p></div>
+      <Card name="yamane" date="2020/12/13" />
+      {sampleArray.map((data) => {
+        console.log(data);
+        return <Card name={data.name} date={data.date} key={data.id} />;
+
+      })}
     </div>
+
   );
 }
 
